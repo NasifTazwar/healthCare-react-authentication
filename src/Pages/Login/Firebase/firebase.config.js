034bc54c23191ec00bcc5f4d90282d028a127146
NasifTazwar <nasiftazwar@gmail.com>
2021-10-19 23:32:34 +0600

    4

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,11 +1,18 @@
 const firebaseConfig = {
 
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
-  };
+  apiKey: "AIzaSyAPi57lrB94UrlPRkQA6G2PJL0o0HkUMLI",
+
+  authDomain: "medicare-39cb8.firebaseapp.com",
+
+  projectId: "medicare-39cb8",
+
+  storageBucket: "medicare-39cb8.appspot.com",
+
+  messagingSenderId: "1087509877263",
+
+  appId: "1:1087509877263:web:72684d7b51e8cffed6497a"
+
+};
+
 
   export default firebaseConfig;
