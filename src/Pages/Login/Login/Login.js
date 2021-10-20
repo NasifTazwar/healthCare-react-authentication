4b98f519f768d8ece1ccb592fd1064048a33acd2
NasifTazwar <nasiftazwar@gmail.com>
2021-10-20 20:11:00 +0600

    final-6

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -56,12 +56,12 @@ const Login = () => {
                 <form onSubmit={handleLoginClick} className="col-lg-6 col-md-12 offset-lg-3">
                     <div className="mb-3">
                         <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                        <input onBlur={handleEmail} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <input onBlur={handleEmail} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                        <input onBlur={handlePassword} type="password" className="form-control" id="exampleInputPassword1"/>
+                        <input onBlur={handlePassword} type="password" className="form-control" id="exampleInputPassword1" required/>
                     </div>
                     
                     <button className="btn btn-outline-dark" type="submit">Login</button> 
